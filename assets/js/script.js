a0208b3671c3e0fe1a847914f9879ddc97b91c6c
treblot <treblotnad@gmail.com>
2023-11-30 09:55:24 -0600

    added request urls, linked .js and updated formatting with bulma

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var requestWeather =
+  "https://api.openweathermap.org/data/2.5/forecast?lat=44.9778&lon=93.2650&appid=5ca2588417484475ffbce359a5fc0533";
+
+var requestCoordinates =
+  "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=5ca2588417484475ffbce359a5fc0533";
+
+// $.ajax({
+//   url: requestUrl,
+//   method: "GET",
+// }).then(function (response) {
+//   console.log(response);
+// });
