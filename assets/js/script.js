6acbe0d7dc22db3409f5ad39bfb7bd0890df3663
treblot <treblotnad@gmail.com>
2023-12-02 13:29:30 -0600

    added additional API requests for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 var apiKey = "e6807d622a5c9a7b8e5e3adb026aa12c";
+var requestCurrent =
+  "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=imperial&appid=" +
+  apiKey;
+
 var requestWeather =
-  "https://api.openweathermap.org/data/2.5/forecast?lat=44.9778&lon=93.2650&appid=e6807d622a5c9a7b8e5e3adb026aa12c";
+  "https://api.openweathermap.org/data/2.5/forecast?lat=44.9778&lon=93.2650&units=imperial&appid=e6807d622a5c9a7b8e5e3adb026aa12c";
 
 var requestCoordinates =
   "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=e6807d622a5c9a7b8e5e3adb026aa12c";
@@ -18,6 +22,19 @@ function searchCity() {
       var cityLon = data[0].lon;
       var cityCountry = data[0].country;
       console.log(data[0]);
+      searchWeather(cityLat, cityLon);
+      searchForecast(cityLat, cityLon);
+    });
+}
+
+function searchWeather(lat, lon) {
+  fetch(requestCurrent)
+    .then(function (response) {
+      if (!response.ok) throw new Error(response.text);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
     });
 }
 
